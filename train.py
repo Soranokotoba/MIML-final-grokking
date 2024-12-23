6988285372bac791264767bc2de93dbfb12f0504
Soranokotoba <131374357+Soranokotoba@users.noreply.github.com>
2024-12-23 19:59:42 +0800

    Fix random seed and add GPU learning

--- train.py
+++ train.py
@@ -7,21 +7,35 @@ from lr_schedule import get_scheduler
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
-import os 
+import os
+import numpy as np
+from tqdm import tqdm
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+def set_seed(seed: int):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    
+    np.random.seed(seed)
+    
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 def train_model():
     train_acc = []
     eval_acc = []
 
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         model.train()
         running_loss = 0.0
         correct_preds = 0
         total_samples = 0
 
         for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
 
             # foward pass
@@ -53,6 +67,7 @@ def train_model():
 
         with torch.no_grad(): 
             for inputs, labels in eval_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 eval_loss += loss.item() * inputs.size(0)
@@ -67,17 +82,28 @@ def train_model():
         eval_accuracy = correct_preds / total_eval_samples
         eval_acc.append(eval_accuracy)
 
-        if not (epoch + 1) % 10:
+        if not (epoch + 1) % 100:
             print(f"Epoch {epoch+1}/{epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}")
             print(f"Evaluation - Loss: {eval_loss:.4f}, Accuracy: {eval_accuracy:.4f}")
+        
+        # early stopping
+        if epoch > 2000 and eval_acc[-2000] > 0.999:
+            break
     return train_acc, eval_acc
 
 
 if __name__ == "__main__":
+    # set random seed
+    set_seed(42)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    # read config file
     config_path = "./config_base.yaml"
     config, config_str = load_config(config_path)
 
     model = get_model(config)
+    model = model.to(device)
+    
     optimizer = get_optimizer(model, config.optim)
     scheduler = get_scheduler(optimizer, config.optim.lr)
     criterion = nn.CrossEntropyLoss()
@@ -86,6 +112,8 @@ if __name__ == "__main__":
     train_loader, eval_loader = get_dataset(**config.train)
 
     train_acc, eval_acc = train_model()
+    np.savez('result.npz', train_acc=train_acc, eval_acc=eval_acc)
+    
     plt.figure()
     t_step = torch.arange(1, epochs+1)
     plt.plot(t_step, train_acc, label="train")
